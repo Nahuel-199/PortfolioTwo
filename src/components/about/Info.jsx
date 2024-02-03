cfefc41eeffbc652aa54b316dccda42befc54117
Nahuel Cernadas <gabrielnahuel96@gmail.com>
2024-02-03 15:29:04 -0300

    add icon html

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -10,13 +10,13 @@ const Info = () => {
         <div className="about__box">
         <i class='bx bx-award about__icon'></i>
             <h3 className="about__title">Experiencia</h3>
-            <span style={{color: darkMode && "rgb(34, 34, 34)"}} className="about__subtitle">2 años Desarrollando</span>
+            <span style={{color: darkMode && "rgb(34, 34, 34)"}} className="about__subtitle">4 años Desarrollando</span>
         </div>
 
         <div className="about__box">
         <i class='bx bx-briefcase-alt about__icon'></i>
             <h3 className="about__title">Completados</h3>
-            <span style={{color: darkMode && "rgb(34, 34, 34)"}} className="about__subtitle">20 + Projectos</span>
+            <span style={{color: darkMode && "rgb(34, 34, 34)"}} className="about__subtitle">30 + Projectos</span>
         </div>
 
         <div className="about__box">
